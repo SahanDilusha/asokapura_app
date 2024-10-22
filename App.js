8496e9a840369294d93cc5b4da8a664e9bb255aa
SahanDilusha <sahancodelygroup@gmail.com>
2024-10-22 23:45:54 +0530

    "Added font imports, updated App.js with font loading and splash screen, updated package.json and package-lock.json, and refactored UserLogin.js with new styles and components."

--- App.js
+++ App.js
@@ -3,20 +3,41 @@ import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import * as NavigationBar from 'expo-navigation-bar';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 import UserLogin from './screens/UserLogin';
 
 const Drawer = createDrawerNavigator();
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+
+  const [loaded, error] = useFonts({
+    'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'),
+    'Roboto-Light': require('./assets/fonts/Roboto-Light.ttf'),
+    'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
+  });
 
   useEffect(() => {
     NavigationBar.setVisibilityAsync('hidden')
   }, []);
 
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="UserLogin">
-        <Drawer.Screen name="UserLogin" component={UserLogin} />
+        <Drawer.Screen name="UserLogin" component={UserLogin} options={{
+          headerShown: false
+        }} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
--- screens/UserLogin.js
+++ screens/UserLogin.js
@@ -1,10 +1,119 @@
 import { StatusBar } from "expo-status-bar";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { ScrollView, TextInput } from "react-native-gesture-handler";
+import { Image } from 'expo-image';
 import { SafeAreaView } from "react-native-safe-area-context";
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import { useState } from "react";
 
 export default function UserLogin() {
-    return(
-        <SafeAreaView>
-            <StatusBar style={"light"} hidden={true}/>
+
+    const [getShow, setShow] = useState(false);
+
+    const Applogo = require("../assets/images/app_logo.png");
+
+    return (
+        <SafeAreaView style={stylesheet.safeAreaView}>
+            <StatusBar style={"light"} hidden={true} />
+            <ScrollView contentContainerStyle={stylesheet.scrollView} showsHorizontalScrollIndicator={false} showsVerticalScrollIndicator={false}>
+                <View style={stylesheet.container}>
+                    <View style={stylesheet.haderView}>
+                        <Image source={Applogo} style={stylesheet.applogo} />
+                        <Text style={stylesheet.title}>Prithipura Communities</Text>
+                    </View>
+                    <View style={stylesheet.mainView}>
+                        <View style={stylesheet.inputView}>
+                            <TextInput style={stylesheet.input} placeholder={"Enter Your Username"} placeholderTextColor={"#d1d1cd"} />
+                            <View style={stylesheet.passwordInputView}>
+                                <TextInput style={stylesheet.input} secureTextEntry={true} placeholder={"Enter Your Password"} placeholderTextColor={"#d1d1cd"} />
+                                <Pressable style={stylesheet.showPasswordPressable}>
+                                    <FontAwesome5 name={getShow ? "eye" : "eye-slash"} size={30} color={"#d1d1cd"} />
+                                </Pressable>
+                            </View>
+                            <Pressable style={stylesheet.pressable}>
+                                <Text style={stylesheet.pressableText}>Sgin In</Text>
+                            </Pressable>
+                        </View>
+                    </View>
+                    <Text style={stylesheet.text1}>2024@www.prithipura.org</Text>
+                </View>
+            </ScrollView>
         </SafeAreaView>
     );
 }
+
+const stylesheet = StyleSheet.create({
+    safeAreaView: {
+        flex: 1,
+    },
+    scrollView: {
+        flexGrow: 1,
+    },
+    container: {
+        flex: 1,
+        backgroundColor: "#171716",
+        alignItems: "center",
+        padding: 20,
+        gap: 50,
+    },
+    mainView: {
+        width: "60%",
+    },
+    applogo: {
+        width: 100,
+        height: 100,
+    },
+    title: {
+        fontSize: 40,
+        textTransform: "uppercase",
+        color: "#ffffff",
+        fontFamily: "Roboto-Bold",
+    },
+    haderView: {
+        alignItems: "center",
+        gap: 20,
+    },
+    inputView: {
+        gap: 50,
+        alignItems: "center",
+    },
+    input: {
+        width: "100%",
+        height: 50,
+        borderBottomWidth: 2,
+        borderColor: "#ffffff",
+        fontFamily: "Roboto-Regular",
+        fontSize: 25,
+        paddingLeft: 10,
+        paddingRight: 45,
+        color: "#d1d1cd",
+    },
+    passwordInputView: {
+        width: "100%",
+    },
+    pressable: {
+        backgroundColor: "#fccb42",
+        width: "100%",
+        justifyContent: "center",
+        alignItems: "center",
+        height: 55,
+        borderRadius: 10,
+    },
+    pressableText: {
+        fontSize: 25,
+        fontFamily: "Roboto-Bold",
+        color: "#171716",
+    },
+    text1: {
+        fontSize: 20,
+        fontFamily: "Roboto-Regular",
+        color: "#d1d1cd",
+    },
+    showPasswordPressable: {
+        padding: 5,
+        justifyContent: "center",
+        alignItems: "center",
+        position: "absolute",
+        alignSelf: "flex-end",
+    },
+});
