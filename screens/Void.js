92261c4e47f7862ae1858d36ba41384438837fec
SahanDilusha <sahancodelygroup@gmail.com>
2024-10-26 17:26:28 +0530

    "Added TextInput, Animated.View, and BlurView components to Void screen, along with new styles and functionality for a modal dialog."

--- screens/Void.js
+++ screens/Void.js
@@ -1,12 +1,16 @@
 import { useState } from 'react';
-import { Alert, Pressable, StyleSheet, Text, View } from 'react-native';
+import { Alert, Pressable, StyleSheet, Text, View, TextInput } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import Nav from '../component/Nav';
 import { FlashList } from '@shopify/flash-list';
+import Animated, { FadeInDown, FadeInUp, FadeOutDown } from 'react-native-reanimated';
+import { BlurView } from 'expo-blur';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function Void({ navigation }) {
     const [getText, setText] = useState('');
+    const [showModel1, setShowModel1] = useState(true);
 
     const data = [
         {},
@@ -64,7 +68,9 @@ export default function Void({ navigation }) {
                     <View style={stylesheet.rowItem}>
                         <Text style={stylesheet.colText}>3000.00</Text>
                     </View>
-                    <Pressable style={stylesheet.rowItem}>
+                    <Pressable style={stylesheet.rowItem} onPress={() => {
+                        setShowModel1(!showModel1);
+                    }}>
                         <Text style={stylesheet.colText}>1000 <Ionicons name="add-circle-sharp" size={20} color="#037ffc" /></Text>
                     </Pressable>
                     <Pressable style={stylesheet.rowItem}>
@@ -82,6 +88,36 @@ export default function Void({ navigation }) {
             <View style={stylesheet.botomView}>
 
             </View>
+
+            {showModel1 && (
+                <Animated.View entering={FadeInUp} exiting={FadeOutDown} style={stylesheet.modelMain}>
+                    <BlurView tint={"dark"} style={stylesheet.modelBlurView}>
+                        <View style={stylesheet.modelBody}>
+                            <Text style={stylesheet.modelHader}>Update Wastage</Text>
+                            <View style={stylesheet.modelNumberInputView}>
+                                <Pressable style={stylesheet.modelPressable}>
+                                    <Ionicons name="add" size={24} color="#ffffff" />
+                                </Pressable>
+                                <TextInput inputMode={"numeric"} placeholder="Enter quantity" style={stylesheet.modelInput} />
+
+                                <Pressable style={stylesheet.modelPressable}>
+                                    <AntDesign name="minus" size={24} color="#ffffff" />
+                                </Pressable>
+                            </View>
+                            <View style={stylesheet.modelBotom}>
+                                <Pressable style={[stylesheet.modelPressableBotom, stylesheet.modelPressableBotomColor1]}>
+                                    <Text style={stylesheet.modelPressableBotomText}>Update</Text>
+                                </Pressable>
+                                <Pressable style={[stylesheet.modelPressableBotom, stylesheet.modelPressableBotomColor2]} onPress={() => {
+                                    setShowModel1(!showModel1);
+                                }}>
+                                    <Text style={stylesheet.modelPressableBotomText}>Cancel</Text>
+                                </Pressable>
+                            </View>
+                        </View>
+                    </BlurView>
+                </Animated.View>
+            )}
 
         </SafeAreaView>
     );
@@ -130,5 +166,73 @@ const stylesheet = StyleSheet.create({
         backgroundColor: "#f0f0f0",
         height: "100%",
         borderWidth: 1,
+    },
+    modelMain: {
+        width: "100%",
+        height: "100%",
+        position: "absolute",
+    },
+    modelBlurView: {
+        width: "100%",
+        height: "100%",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    modelBody: {
+        minWidth: "50%",
+        minHeight: 200,
+        backgroundColor: "#ffffff",
+        padding: 10,
+    },
+    modelHader: {
+        fontSize: 20,
+        fontFamily: "Roboto-Bold",
+    },
+    modelNumberInputView: {
+        flexDirection: 'row',
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 10,
+        marginTop: 20,
+    },
+    modelPressable: {
+        width: 50,
+        height: 50,
+        backgroundColor: "#000",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 10,
+    },
+    modelInput: {
+        width: 150,
+        textAlign: "center",
+        fontFamily: "Roboto-Regular",
+        fontSize: 20,
+        color:"#000000",
+    },
+    modelBotom: {
+        flexDirection: 'row',
+        marginTop: 30,
+        gap: 10,
+        justifyContent: "flex-end",
+        alignItems: "center",
+    },
+    modelPressableBotom: {
+        justifyContent: "center",
+        alignItems: "center",
+        width: 100,
+        height: 50,
+        borderRadius: 10,
+    },
+    modelPressableBotomText: {
+        fontSize: 18,
+        fontFamily: "Roboto-Bold",
+        color: "#ffffff",
+    },
+    modelPressableBotomColor1: {
+        backgroundColor: "#000000",
+    },
+    modelPressableBotomColor2: {
+        backgroundColor: "#fc2b4a",
     },
 });
